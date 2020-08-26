4d1d600ab9016229d34fa410c7812b4c528a8418
Key Coder <gauravddja@gmail.com>
2020-08-26 14:03:53 +0530

    Create IDEA2020.sh

---
+++ IDEA2020.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine; do
+for product in IntelliJIdea ; do
+  echo "Resetting trial period for $product"
+
+  echo "removing evaluation key..."
+  rm -rf ~/Library/Preferences/$product*/eval
+
+  # Above path not working on latest version. Fixed below
+  rm -rf ~/Library/Application\ Support/JetBrains/$product*/eval
+
+  echo "removing all evlsprt properties in options.xml..."
+  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
+
+  # Above path not working on latest version. Fixed below
+  sed -i '' '/evlsprt/d' ~/Library/Application\ Support/JetBrains/$product*/options/other.xml
+
+  echo
+done
+
+echo "removing additional plist files..."
+rm -f ~/Library/Preferences/com.apple.java.util.prefs.plist
+rm -f ~/Library/Preferences/com.jetbrains.*.plist
+rm -f ~/Library/Preferences/jetbrains.*.*.plist
+killall cfprefsd
+echo
+echo "That's it, enjoy ;)"
